9fda69f68b3243604605c0f45e8995d55a284214
Zach Sisco <zach@zsisco.net>
2023-06-04 10:45:13 -0700

    regen photo galleries using meihua

--- scripts/meihua.sh
+++ scripts/meihua.sh
@@ -3,7 +3,7 @@
 #
 
 if [ $# -eq 0 ]; then
-	printf "Usage:\n $ meihua <photo/dir/path>\n\n"
+	printf "\nUsage:\n $ meihua.sh image-file ...\n\n"
 	exit 1
 fi
 
@@ -11,34 +11,37 @@ photo_dir=$1
 
 read -p "Gallery title: " gallery_title
 read -p "Gallery date: " gallery_date
-read -p "Gallery filename: " gallery_page
 
-# Generate <img> tags based on files in photo dir
+# Check if ImageMagick installed
+(! command -v identify &> /dev/null)
+has_identify=$?
+
+# Generate <img> elements based on input files
 imgs=""
-for photo in ${photo_dir}/*; do
-	# Check if ImageMagick installed
-	if ! command -v identify &> /dev/null;
+for photo; do
+	if [ -z "$has_identify" ];
 	then
 		imgs+="\t<p><img src=\"$photo\"></p>\n"
 	else
-		ratio=`identify -format '%[fx:(h/w)]' "$photo"`
+		# Use ImageMagick identify to get height/width of image
+		ratio=$(identify -format '%[fx:(h/w)]' "$photo")
 		if awk "BEGIN {exit !($ratio <= 1.0)}";
 		then
 			imgs+="\t<p><img src=\"$photo\" class=\"landscape\"></p>\n"
 		else
 			imgs+="\t<p><img src=\"$photo\" class=\"portrait\"></p>\n"
 			# TODO: For half-frame photos it could be cool to have two
-			# next to each other on a single line.
+			# photos next to each other on a single <p>.
 		fi
 	fi
 done
 
-cat <<EOF > ${gallery_page}.html
+cat <<EOF > index.html
 <!DOCTYPE html>
 <html>
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes" />
-	<title>$gallery_title</title>
+	<title>${gallery_title}</title>
 	<style>
 		img {
 			object-fit: contain;
@@ -60,9 +63,6 @@ cat <<EOF > ${gallery_page}.html
 			padding: 0.62em;
 			font: 1.0em/1.6 monospace;
 		}
-		h1,h2,h3 {
-			line-height: 1.2;
-		}
 		header {
 			margin-bottom: 4em;
 			text-align: center;
@@ -71,10 +71,10 @@ cat <<EOF > ${gallery_page}.html
 </head>
 <body>
 	<header>
-		<h1>$gallery_title</h1>
-		<h3>$gallery_date</h3>
+		<h1>${gallery_title}</h1>
+		<h3>${gallery_date}</h3>
 	</header>
-`echo $imgs`
+$(echo "$imgs")
 </body>
 </html>
 EOF
