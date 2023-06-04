d0b3c1be8fe44a2f7085aa8a76e0f7f204b5bef9
Zach Sisco <zach@zsisco.net>
2023-06-03 23:19:38 -0700

    photo gallery script meihua

---
+++ scripts/meihua.sh
@@ -0,0 +1,77 @@
+#! /bin/sh
+# 美画
+#
+
+if [ $# -eq 0 ]; then
+	printf "Usage:\n $ meihua <photo/dir/path>\n\n"
+	exit 1
+fi
+
+photo_dir=$1
+
+read -p "Gallery title: " gallery_title
+read -p "Gallery date: " gallery_date
+
+# Generate <img> tags based on files in photo dir
+imgs=""
+for photo in ${photo_dir}/*; do
+	# Check if ImageMagick installed
+	if ! command -v identify &> /dev/null;
+	then
+		imgs+="\t<p><img src=\"$photo\"></p>\n"
+	else
+		ratio=`identify -format '%[fx:(h/w)]' "$photo"`
+		if awk "BEGIN {exit !($ratio <= 1.0)}";
+		then
+			imgs+="\t<p><img src=\"$photo\" class=\"landscape\"></p>\n"
+		else
+			imgs+="\t<p><img src=\"$photo\" class=\"portrait\"></p>\n"
+		fi
+	fi
+done
+
+cat <<EOF > gallery.html
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes" />
+	<style>
+		img {
+			object-fit: contain;
+			display: block;
+			margin-left: auto;
+			margin-right: auto;
+		}
+		.landscape {
+			width: 100%;
+			height: auto;
+		}
+		.portrait {
+			width: 60%;
+			height: auto;
+		}
+		body {
+			margin: 1em auto;
+			max-width: 60%;
+			padding: 0.62em;
+			font: 1.0em/1.6 monospace;
+		}
+		h1,h2,h3 {
+			line-height: 1.2;
+		}
+		header {
+			margin-bottom: 4em;
+			text-align: center;
+		}
+	</style>
+</head>
+<body>
+	<header>
+		<h1>$gallery_title</h1>
+		<h3>$gallery_date</h3>
+	</header>
+`echo $imgs`
+</body>
+</html>
+EOF
+
